6516bd26b90e62f9ac5a0a1d38eb9c0b0c909583
OmarMWarraich <earthling986@gmail.com>
2023-06-17 21:50:02 +0500

    feat: Middleware

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
+const notFound = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/errorHandlerMiddleware');
 
 /* Middleware */
 app.use(express.static('./public'));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+/* Routes */
+
+app.use('/api/v1/tasks', require('./routes/tasks'));
+
+/* Middleware */
+
+app.use(notFound);
+app.use(errorMiddleware);
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    } catch (error) {
+        console.log(error);
     }
-);
+};
 
-
-app.listen(5000, () => console.log('Server running on port 5000'));
+start();
